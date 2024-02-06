00f0dd28af6097e4dcc5bb44218d30859481c605
Dima Vanichkin <dima_one@ukr.net>
2024-02-07 00:20:45 +0200

    feature: edit Main

--- src/main/java/org/twentymethods/Main.java
+++ src/main/java/org/twentymethods/Main.java
@@ -2,6 +2,6 @@ package org.twentymethods;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Dobyi den everybody!");
     }
 }
