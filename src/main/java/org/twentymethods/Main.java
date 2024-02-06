38e0a19b1944cf72aef86e7d3a417f907598ca66
Dima Vanichkin <dima_one@ukr.net>
2024-02-07 00:41:21 +0200

    JEP-28 refactor

--- src/main/java/org/twentymethods/Main.java
+++ src/main/java/org/twentymethods/Main.java
@@ -2,6 +2,7 @@ package org.twentymethods;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Dobyi den everybody!");
+        System.out.println("Dobryi den everybody!");
+        System.out.println("Dobryi den everybody!");
     }
 }
