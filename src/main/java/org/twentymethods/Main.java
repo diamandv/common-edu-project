8ff6a716792a164143d2ffce45b8729a5a123c88
Comp <dima_one@ukr.net>
2024-02-26 15:51:02 +0200

    test commit 3

--- src/main/java/org/twentymethods/Main.java
+++ src/main/java/org/twentymethods/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Dobryi den everybody!");
         System.out.println("Dobryi den everybody!");
         System.out.println("Dobryi den everybody!");
+        System.out.println("Dobryi den everybody!");
     }
 }
