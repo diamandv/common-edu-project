96a6cef1170c1669dbeae85d55a1f74bf39792c4
Comp <dima_one@ukr.net>
2024-02-26 16:54:48 +0200

    CU-86bxmqqv1 - Task name - Second commit

--- src/main/java/org/twentymethods/Main.java
+++ src/main/java/org/twentymethods/Main.java
@@ -3,13 +3,5 @@ package org.twentymethods;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Dobryi den everybody!");
-        System.out.println("Dobryi den everybody!");
-        System.out.println("Dobryi den everybody!");
-        System.out.println("Dobryi den everybody!");
-        System.out.println("Dobryi den everybody!");
-        System.out.println("Dobryi den everybody!");
-        System.out.println("Dobryi den everybody!");
-        System.out.println("Dobryi den everybody!");
-        System.out.println("Dobryi den everybody!");
     }
 }
