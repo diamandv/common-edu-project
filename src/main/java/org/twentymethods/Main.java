aefbd033008cff96d5d6a1fe072fa6b37b5c5438
Comp <dima_one@ukr.net>
2024-02-26 15:53:09 +0200

    CU-86bxmqqv1 - Task name

--- src/main/java/org/twentymethods/Main.java
+++ src/main/java/org/twentymethods/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Dobryi den everybody!");
         System.out.println("Dobryi den everybody!");
         System.out.println("Dobryi den everybody!");
+        System.out.println("Dobryi den everybody!");
     }
 }
