76170e12ef30219b46463f3e7ed0b562d56ee663
Comp <dima_one@ukr.net>
2024-02-26 17:09:26 +0200

    CU-86bxmqqv1 - Third commit

--- src/main/java/org/twentymethods/Main.java
+++ src/main/java/org/twentymethods/Main.java
@@ -3,5 +3,7 @@ package org.twentymethods;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Dobryi den everybody!");
+        System.out.println("Dobryi den everybody!");
+        System.out.println("Dobryi den everybody!");
     }
 }
